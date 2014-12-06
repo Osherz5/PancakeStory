86d0be8489cf1c50dfb6e00541744cbadcdd583a
breverie <zarad.avishay@gmail.com>
2014-12-06 22:52:00 +0200

    refactored eventjs
    standartized osher's code to work with prototype instead of
    newbheritence.

--- client/js/game/event.js
+++ client/js/game/event.js
@@ -1,33 +1,39 @@
 
 
-ScriptedEvent = function(game, name, data) {
+var ScriptedEvent = function(game, name, data) {
     this.data = game.load.text(name,data);
-
-    this.runOn = function(target) {
-        // object needs to be pixelEntity
-        this.target = target;
-        this.commands = JSON.parse(game.cache.getText(name));
-        this.currentCommandIndex = 0;
-        console.log("event fired!");
-    }
-
-    this.update = function() {
-        if (typeof this.currentCommandIndex == 'undefined')
-            return;
-
-        command = this.commands.steps[this.currentCommandIndex];
-
-        switch(command['action']){
-            case 'move':
-                dest = [command['params'][0],command['params'][1]];
-                speed = command['params'][2];
-
-                this.target.sprite.body.moveRight(speed*Math.sign(dest[0]));
-                this.target.sprite.body.moveDown(speed*Math.sign(dest[1]));
-                break;
-        }
-    }
-
+    this.name = name;
+    this.isRunning = false;
 };
 
 
+ScriptedEvent.prototype.runOn = function(target) {
+    // object needs to be pixelEntity
+    this.target = target;
+    this.commands = JSON.parse(game.cache.getText(this.name));
+    this.currentCommandIndex = 0;
+    this.isRunning = true;
+    console.log("event fired!");
+};
+
+ScriptedEvent.prototype.update = function() {
+    if (typeof this.currentCommandIndex === 'undefined') {
+        return;
+    }
+
+    if (!this.isRunning) {
+        return;
+    }
+
+    var command = this.commands.steps[this.currentCommandIndex];
+
+    switch(command.action){
+        case 'move':
+            var dest = [command.params[0], command.params[1]];
+            var speed = command.params[2];
+
+            this.target.sprite.body.moveRight(speed * Math.sign(dest[0]));
+            this.target.sprite.body.moveDown(speed * Math.sign(dest[1]));
+            break;
+    }
+};
