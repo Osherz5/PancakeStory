bd512f869e872cbbd5ef53bab04cf25527bb8170
Osherz5 <Osherz5@gmail.com>
2014-12-06 22:03:09 +0159

    Add working movement, and dialog events

--- client/js/game/event.js
+++ client/js/game/event.js
@@ -12,18 +12,34 @@ ScriptedEvent = function(game, name, data) {
     }
 
     this.update = function() {
-        if (typeof this.currentCommandIndex == 'undefined')
+        if (typeof this.currentCommandIndex == 'undefined' || this.currentCommandIndex > this.commands.steps.length)
             return;
 
         command = this.commands.steps[this.currentCommandIndex];
 
         switch(command['action']){
             case 'move':
+                
                 dest = [command['params'][0],command['params'][1]];
                 speed = command['params'][2];
 
-                this.target.sprite.body.moveRight(speed*Math.sign(dest[0]));
-                this.target.sprite.body.moveDown(speed*Math.sign(dest[1]));
+
+                // moves target towards destination
+
+                this.target.sprite.body.moveRight(speed*Math.sign(dest[0]-this.target.sprite.x));
+                this.target.sprite.body.moveDown(speed*Math.sign(dest[1]-this.target.sprite.y));
+
+                // check if command ended
+
+                if (Math.round(this.target.sprite.x) == Math.round(dest[0]) && Math.round(this.target.sprite.y) == Math.round(dest[1])) {
+                    console.log("ended command");
+                    this.target.sprite.body.setZeroVelocity();
+                    this.currentCommandIndex++;
+                }
+                break;
+            case 'say':
+                hud.say('Unknown', command['params'][0]);
+
                 break;
         }
     }
