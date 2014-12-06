3749f4c341e0a060328ef9d50fb549c87f4b2661
funerr(agam) <agam360@gmail.com>
2014-12-06 23:53:15 +0200

    Conventions in events

--- client/js/game/event.js
+++ client/js/game/event.js
@@ -1,54 +1,54 @@
 
 
-ScriptedEvent = function(game, name, data) {
+var ScriptedEvent = function(game, name, data) {
     this.data = game.load.text(name,data);
+}
 
-    this.runOn = function(target) {
-        // object needs to be pixelEntity
-        this.target = target;
-        this.commands = JSON.parse(game.cache.getText(name));
-        this.currentCommandIndex = 0;
-        this.waitOnUserInput = false;
-        console.log("event fired!");
+ScriptedEvent.prototype.runOn = function(target) {
+    // object needs to be pixelEntity
+    this.target = target;
+    this.commands = JSON.parse(game.cache.getText(name));
+    this.currentCommandIndex = 0;
+    this.waitOnUserInput = false;
+    console.log("event fired!");
+}
+
+ScriptedEvent.prototype.update = function() {
+    if (typeof this.currentCommandIndex == 'undefined' 
+        || this.currentCommandIndex > this.commands.steps.length 
+        || this.waitOnUserInput) {
+        return;
     }
 
-    this.update = function() {
-        if (typeof this.currentCommandIndex == 'undefined' || this.currentCommandIndex > this.commands.steps.length || this.waitOnUserInput)
-            return;
+    var command = this.commands.steps[this.currentCommandIndex];
 
-        command = this.commands.steps[this.currentCommandIndex];
+    switch(command['action']) {
+        case 'move':
+            
+            var dest = [command['params'][0],command['params'][1]];
+            var speed = command['params'][2];
 
-        switch(command['action']){
-            case 'move':
-                
-                dest = [command['params'][0],command['params'][1]];
-                speed = command['params'][2];
+            // moves target towards destination
+            this.target.sprite.body.moveRight(speed*Math.sign(dest[0]-this.target.sprite.x));
+            this.target.sprite.body.moveDown(speed*Math.sign(dest[1]-this.target.sprite.y));
 
+            // check if command ended
+            if (Math.round(this.target.sprite.x) == Math.round(dest[0]) 
+                && Math.round(this.target.sprite.y) == Math.round(dest[1])) {
+                //console.log("ended command");
+                this.target.sprite.body.setZeroVelocity();
+                this.currentCommandIndex++;
+            }
+            break;
+        case 'say':
+            this.waitOnUserInput = true;
+            hud.say(command['params'][0], command['params'][1], function sayCallback() {
+                this.currentCommandIndex++;
+                this.waitOnUserInput = false;
+            });
 
-                // moves target towards destination
-
-                this.target.sprite.body.moveRight(speed*Math.sign(dest[0]-this.target.sprite.x));
-                this.target.sprite.body.moveDown(speed*Math.sign(dest[1]-this.target.sprite.y));
-
-                // check if command ended
-
-                if (Math.round(this.target.sprite.x) == Math.round(dest[0]) && Math.round(this.target.sprite.y) == Math.round(dest[1])) {
-                    //console.log("ended command");
-                    this.target.sprite.body.setZeroVelocity();
-                    this.currentCommandIndex++;
-                }
-                break;
-            case 'say':
-                this.waitOnUserInput = true;
-                hud.say(command['params'][0], command['params'][1], function() {
-                    this.currentCommandIndex++;
-                    this.waitOnUserInput = false;
-                });
-
-                break;
-        }
+            break;
     }
-
-};
+}
 
 
