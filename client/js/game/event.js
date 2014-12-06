c3c67f7e7b6f64e5477c0704c811e7e91f9e1604
Osherz5 <Osherz5@gmail.com>
2014-12-06 21:32:07 +0159

    add command parsing for events

--- client/js/game/event.js
+++ client/js/game/event.js
@@ -2,20 +2,30 @@
 
 ScriptedEvent = function(game, name, data) {
     this.data = game.load.text(name,data);
+
     this.runOn = function(target) {
         // object needs to be pixelEntity
         this.target = target;
         this.commands = JSON.parse(game.cache.getText(name));
-        this.commands.steps.forEach(function(command) {
-            
-        });
+        this.currentCommandIndex = 0;
         console.log("event fired!");
     }
 
     this.update = function() {
-        if (typeof this.target == 'undefined')
+        if (typeof this.currentCommandIndex == 'undefined')
             return;
-        this.target.sprite.body.velocity.x = 1;
+
+        command = this.commands.steps[this.currentCommandIndex];
+
+        switch(command['action']){
+            case 'move':
+                dest = [command['params'][0],command['params'][1]];
+                speed = command['params'][2];
+
+                this.target.sprite.body.moveRight(speed*Math.sign(dest[0]));
+                this.target.sprite.body.moveDown(speed*Math.sign(dest[1]));
+                break;
+        }
     }
 
 };
